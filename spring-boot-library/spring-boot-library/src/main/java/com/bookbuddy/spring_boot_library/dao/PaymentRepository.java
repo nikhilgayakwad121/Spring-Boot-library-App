629c35a315742922df4e4c2047d89e3e2dd8e5c3
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-09-18 01:55:19 +0530

    Added final commit to library spring boot project

---
+++ spring-boot-library/spring-boot-library/src/main/java/com/bookbuddy/spring_boot_library/dao/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.bookbuddy.spring_boot_library.dao;
+
+import com.bookbuddy.spring_boot_library.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Payment findByUserEmail(String userEmail);
+}
+
---
+++ spring-boot-library/spring-boot-library/src/main/java/com/bookbuddy/spring_boot_library/responsemodels/ShelfCurrentLoansResponse.java
@@ -0,0 +1,17 @@
+package com.bookbuddy.spring_boot_library.responsemodels;
+
+import com.bookbuddy.spring_boot_library.entity.Book;
+import lombok.Data;
+
+@Data
+public class ShelfCurrentLoansResponse {
+
+    public ShelfCurrentLoansResponse(Book book, int daysLeft) {
+        this.book = book;
+        this.daysLeft = daysLeft;
+    }
+
+    private Book book;
+
+    private int daysLeft;
+}
